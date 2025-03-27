ce1e8c387ad2c7f883775433f57eb49f2d2ec12d
dztpadm <admin@dztpp.ru>
2025-03-27 16:50:48 +0300

    Commit_001

---
+++ higo.go
@@ -0,0 +1,8 @@
+// Пакет с функцией приветствия.
+package higo
+
+// Объявление функции, которая возвращает строку:
+func Hi() string {
+    str := "<h1>Привет, Pascal/Assembly/C/Go/JS!"
+    return str
+}
